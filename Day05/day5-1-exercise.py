3b0e218af95c1db0234419e65afc2ade6e70fed0
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-01-05 06:14:42 +0530

    done with day 5

---
+++ Day05/day5-1-exercise.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+# import random
+
+total_height = 0
+student = 0
+
+for height in student_heights:
+    total_height += height
+    student += 1
+
+print(round(total_height/student))
+
+

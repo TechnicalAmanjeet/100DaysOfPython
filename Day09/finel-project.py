051e32fa5a9404b884c58b3c7db0727a5390d6db
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-01-10 08:48:35 +0530

    done with 1st exercise of day 9

--- Day09/finel-project.py
+++ Day09/finel-project.py
@@ -10,9 +10,23 @@ def sailent_bid():
     user_name = input("What is Your Name? : ")
     user_bid = int(input("what's Your bid? : $"))
     bid.update({user_name:user_bid})
-    print(bid)
+    # print(bid)
+
+# finding the winner and print it on the console
+def winner(bid):
+    max_bid = 0
+    max_bid_biders = ""
+
+    for biders,bid in bid.items():
+        if bid > max_bid:
+            max_bid = bid
+            max_bid_biders = biders
+
+    print(f"\n\n The winner is {max_bid_biders} and his/her bid is ${max_bid}")
 
 
+
+# execution of code starts from here
 
 print("Welcome to the secret auction game")
 sailent_bid()
@@ -20,19 +34,14 @@ sailent_bid()
 while True:
     is_bieders = input("Are There any other bider's? Type 'yes' or 'no' : ")
     if is_bieders == 'yes':
-        # os.system('clear')
+        os.system('cls')
         sailent_bid()
     elif is_bieders == 'no':
         break
     else:
         print("please write spelling correctly!!")
 
-max_bid = 0
-max_bid_biders = ""
+# calling winner function
+winner(bid)
 
-for biders,bid in bid.items():
-    if bid > max_bid:
-        max_bid = bid
-        max_bid_biders = biders
-
-print(f"The winner is {max_bid_biders} and his/her bid is ${max_bid}")
+print("\n\n Thank you so much for playing this game")

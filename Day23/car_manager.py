6cf11a51ba5adb932ef6761d9aeb028916e9479b
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-10 02:13:37 +0530

    done with car manager class

--- Day23/car_manager.py
+++ Day23/car_manager.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+STARTING_POS_X = 330
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.setheading(180)
+        self.color(random.choice(COLORS))
+        starting_pos_y = random.randint(-220, 220)
+        self.setposition(STARTING_POS_X, starting_pos_y)
+        self.move_increment = STARTING_MOVE_DISTANCE
+
+    def move(self):
+        """move car by some distance."""
+        self.fd(self.move_increment)
--- Day23/main.py
+++ Day23/main.py
@@ -11,14 +11,35 @@ screen.tracer(0)
 # creating object of player
 player = Player()
 
+car_manage = []
+# creating object of car_manager
+car = CarManager()
+car_manage.append(car)
+
+
 screen.listen()
 screen.onkey(player.up, "Up")
 
 
 game_is_on = True
+no_loops = 0
+# print(type(car_manage))
+
 while game_is_on:
     # check if player touches the finel line then level up by 1.
     if player.is_touch_finish_line():
         print("level up")
+
+    # move each car by some distance
+    for car in car_manage:
+        car.move()
+
     time.sleep(0.1)
     screen.update()
+    no_loops += 1
+
+    # after every 6 times loop runs create one new object of car.
+    if no_loops == 6:
+        car = CarManager()
+        car_manage.append(car)
+        no_loops = 0

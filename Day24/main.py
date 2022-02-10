031e80e13e77f0eb7f9de0a4fc9d6303eeb8c1c9
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-10 23:34:27 +0530

    learn how to read data from existing file. how to create new file and how to write and append data into it.

--- Day24/main.py
+++ Day24/main.py
@@ -1,7 +1,28 @@
-# worst way to work with file.
+# # worst way to work with file.
+#
+# file = open("file.txt")
+# content = file.read()
+# print(content)
+#
+#
 
-file = open("file.txt")
-content = file.read()
-print(content)
+# Best way to work with file.
+with open("file.txt", 'w') as file: # first it creates a file with file.txt filename if it does not exist.
+    # print(file.read())
+    data = "hii Guy's, I'm Amanjeet Kumar."
+    file.write(data)
 
+with open("file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("file.txt", 'a') as file: # append data into the file.
+    data = "\nI born and brought up in Bihar."
+    file.write(data)
+
+print("\nAfter appending the data : \n")
+
+with open("file.txt") as file:
+    contents = file.read()
+    print(contents)
 

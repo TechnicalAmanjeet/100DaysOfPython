b2841316a6e7a52080d3234c34e53e526132d17f
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-11 17:05:54 +0530

    extracted day, temp, condition from a weather csv file

--- Day25/main.py
+++ Day25/main.py
@@ -16,7 +16,28 @@ import csv
 #     csv_writer.writerow(data)
 
 # read data from csv file.
-with open("weather_data.csv") as csv_file:
-    c = csv.reader(csv_file)
-    for data in c:
-        print(data)
+# with open("weather_data.csv") as csv_file:
+#     c = csv.reader(csv_file)
+#     for data in c:
+#         print(data)
+
+# extract temp data from weather data csv file.
+with open("./weather_data.csv") as csv_file:
+    temp = []
+    day = []
+    condition = []
+    index = 0
+    csv_data = csv.reader(csv_file)
+    for row in csv_data:
+        if index == 0:
+            index_of_day = row.index("day")
+            index_of_temp = row.index("temp")
+            index_of_condition = row.index("condition")
+            # print(index_of_temp)
+            index += 1
+        else:
+            temp.append(row[index_of_temp])
+            day.append(row[index_of_day])
+            condition.append(row[index_of_condition])
+    print(day, temp, condition)
+

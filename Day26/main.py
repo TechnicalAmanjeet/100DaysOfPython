5d91f9db69147e4e665942bc73f19707bf4dc3ee
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-13 23:21:47 +0530

    done with challange 2

--- Day26/main.py
+++ Day26/main.py
@@ -1,11 +1,13 @@
 # ********** List comprehension challenge **********
 
 # challange 1.
+import random
+
 string = "Amanjeet"
 new_list = [alphabet for alphabet in string]
-# print(len(new_list))
+print(len(new_list))
 
-#challange 2
+# challange 2
 new_list = [number * 2 for number in range(0, 11)]
 # print(new_list)
 
@@ -17,3 +19,16 @@ new_names_list = [name.upper() for name in names if len(name) > 3]
 
 
 # ******** Dict comprehension challenge *********
+
+# Challenge 1
+students = ["Amanjeet", "Kajal", "Riya", "Neha", "abp", "kismat", "Anjali"]
+
+students_score_card = {student: random.randint(0, 100) for student in students}
+print(students_score_card)
+
+# challenge 2
+passed_students = [student for (student, student_score) in students_score_card.items() if student_score >= 60]
+print(passed_students)
+
+# for (student, score) in students_score_card.items():
+#     print(f"{student} : {score}")

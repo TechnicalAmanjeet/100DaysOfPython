23e929192806abfc2ddba063e6f67fa93b23038a
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-13 22:22:38 +0530

    done with 3rd challenge

--- Day26/main.py
+++ Day26/main.py
@@ -6,3 +6,9 @@ new_list = [alphabet for alphabet in string]
 #challange 2
 new_list = [number * 2 for number in range(0, 11)]
 print(new_list)
+
+# challenge 3.
+names = ["Amanjeet", "Kajal", "Riya", "Neha", "abp", "kismat", "Anjali", "a", "b"]
+
+new_names_list = [name.upper() for name in names if len(name) > 3]
+print(new_names_list)

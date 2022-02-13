6a202d2b73afd44d257cbdc31b218c37b13d8065
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-13 22:17:42 +0530

    done with first two challange of day 26

---
+++ Day26/main.py
@@ -0,0 +1,8 @@
+# challange 1.
+string = "Amanjeet"
+new_list = [alphabet for alphabet in string]
+# print(len(new_list))
+
+#challange 2
+new_list = [number * 2 for number in range(0, 11)]
+print(new_list)
